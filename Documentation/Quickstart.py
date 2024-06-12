9c2c50cef01568993b81de3fa08dfc75c711f09b
tarunshrestha <tarunshrestha90@gmail.com>
2024-06-12 14:19:37 +0545

    tensors

--- Documentation/Quickstart.py
+++ Documentation/Quickstart.py
@@ -11,7 +11,7 @@ from torchvision.transforms import ToTensor
 training_data = datasets.FashionMNIST(
     root='data',
     train=True,
-    download=True,
+    download=False,
     transform=ToTensor(),
 )
 
@@ -19,17 +19,136 @@ training_data = datasets.FashionMNIST(
 test_data = datasets.FashionMNIST(
     root='data',
     train=False,
-    download=True,
+    download=False,
     transform= ToTensor(), 
 )
 
 batch_size = 64
 
 # Create data loaders.
-training_dataloader = DataLoader(training_data, batch_size=batch_size)
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
 for X, y in test_dataloader:
     print("Shape of X [N, C, H, W]:",X.shape)
     print("Shape of y:",y.shape, y.dtype)
     break
+
+# Create Models
+# To define a neural network in PyTorch, 
+# we create a class that inherits from nn.Module.
+
+# Get cpu, gpu or mps device for training. 
+device = (
+    'cuda'
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f'Device: {device}')
+
+# Define model
+class NeuralNetwork(nn.ModuleDict):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+    
+model = NeuralNetwork().to(device)
+print(model)
+
+
+# Optomizing the Model Parameters
+ # To train a model, we need a loss function and an optimizer.
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+#In a single training loop, the model makes predictions on the training dataset (fed to it in batches), 
+# and backpropagates the prediction error to adjust the model’s parameters.
+
+def train(dataloder, model, loss_fn, optimizer):
+    size = len(dataloder.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloder):
+        X, y = X.to(device), y.to(device)
+
+        # Computing prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad() 
+
+        if batch % 100 ==0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}")
+
+epochs = 5
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
+
+
+# Saving Models
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
+
+
+# Load Models
+
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load('model.pth'))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
--- Documentation/test.py
+++ Documentation/test.py
@@ -4,3 +4,8 @@ print(x)
 
 
 print(torch.cuda.is_available())
+
+# def addi(a, b):
+#     return 'yes' if a+b == 6 else 'no'
+
+# print(addi(2, 4))
